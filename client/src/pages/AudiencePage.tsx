972b3881ca5510aa61ee4fe8c4b0c34221f0bc6b
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-06-11 02:37:18 +0530

    connected the audience page with the backend.

--- client/src/pages/AudiencePage.tsx
+++ client/src/pages/AudiencePage.tsx
@@ -1,10 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CreateAudienceForm } from "../components/CreateAudienceForm";
 import AudienceCard from "../components/AudienceCard";
 
+interface AudienceType {
+  _id: string;
+  name: string;
+  createdAt: string;
+  size: number;
+}
+
 const AudiencePage = () => {
   const [showForm, setShowForm] = useState(false);
+  const [audiences, setAudiences] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:3000/audience/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setAudiences(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching audience data:", error);
+      });
+  }, []);
+  
   const handleCreateAudience = () => {
     setShowForm(true);
   };
@@ -23,11 +42,14 @@ const AudiencePage = () => {
       </button>
       {showForm && <CreateAudienceForm handleCloseForm={handleCloseForm} />}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <AudienceCard
-          audienceName="Sample Name"
-          createdAt="12 June"
-          size={10}
-        />
+        {audiences.map((audience: AudienceType) => (
+          <AudienceCard
+            key={audience._id}
+            audienceName={audience.name}
+            createdAt={new Date(audience.createdAt).toLocaleDateString()}
+            size={audience.size}
+          />
+        ))}
       </div>
     </div>
   );
