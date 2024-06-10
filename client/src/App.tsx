9df5f5de2e521611b1aad889a7a4a0fe6ba26045
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-06-11 02:25:38 +0530

    added the message logs page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AudiencePage from "./pages/AudiencePage";
+import AudienceDetailPage from "./pages/AudienceDetailPage";
+import MessageLogsPage from "./pages/MessageLogsPage";
 
 const App = () => {
   return (
-    <div>
-
+    <div className="container mx-auto">
+      <Router>
+        <Routes>
+          <Route path="/" element={<AudiencePage />} />
+          <Route path="/:audienceId" element={<AudienceDetailPage />} />
+          <Route path="/logs/:audienceId" element={<MessageLogsPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
